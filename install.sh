354df3ba53be3315b26f7b08fa325de96d46b94c
Gabriel Munteanu <wyrdagarjzla@yahoo.com>
2018-10-05 22:19:29 +0200

    Auto aprove apt install questions

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 
 # prepare for xrdp
-sudo apt-get -q install git autoconf libtool pkg-config gcc g++ make  libssl-dev libpam0g-dev libjpeg-dev libx11-dev libxfixes-dev libxrandr-dev  flex bison libxml2-dev intltool xsltproc xutils-dev python-libxml2 g++ xutils libfuse-dev libmp3lame-dev nasm libpixman-1-dev xserver-xorg-dev
+sudo apt-get -q -y install git autoconf libtool pkg-config gcc g++ make  libssl-dev libpam0g-dev libjpeg-dev libx11-dev libxfixes-dev libxrandr-dev  flex bison libxml2-dev intltool xsltproc xutils-dev python-libxml2 g++ xutils libfuse-dev libmp3lame-dev nasm libpixman-1-dev xserver-xorg-dev
 
 
 # prepare for xfce
-sudo apt -q install xorg xfce4 xfce4-terminal tango-icon-theme dbus-x11
+sudo apt -q -y install xorg xfce4 xfce4-terminal tango-icon-theme dbus-x11
 echo xfce4-session > ~/.xsession
 
 # install xrdp
